4d72d8909181bd966199d5a1cba7f65fc06bdb47
Viktoria Bashari <102989946+VBashari@users.noreply.github.com>
2023-10-11 15:59:42 +0200

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,21 @@
+module BookstoreRE {
+	requires javafx.base;
+	requires javafx.graphics;
+    requires transitive javafx.controls;
+    requires javafx.media;
+    requires javafx.web;
+	requires java.desktop;
+	
+    exports views;
+    exports views.employees;
+    exports views.books;
+    exports views.stats;
+    
+    exports controllers;
+    exports controllers.books;
+    
+    exports exceptions;
+    
+    exports models;
+    exports models.helpers;
+}
